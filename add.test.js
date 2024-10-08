381c9aa044fbac468d8d2784a1884ef3ffab36c8
abhinav khanduja <abhinavkhanduja@abhinavs-MacBook-Pro.local>
2024-10-08 14:07:51 +0530

    test: should output 0

--- add.test.js
+++ add.test.js
@@ -9,4 +9,9 @@ describe("add", function () {
   it("should add positive numbers with delimiter", function () {
     expect(add("//|1|2\n|3\n|10")).to.equal(16);
   });
+
+  it("should output 0", function () {
+    expect(add("//|")).to.equal(0);
+    expect(add("")).to.equal(0);
+  });
 });
