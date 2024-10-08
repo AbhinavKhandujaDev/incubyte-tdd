fb53dfb8a34d3b2ebf1946ac04f1acb9e5f0819e
abhinav khanduja <abhinavkhanduja@abhinavs-MacBook-Pro.local>
2024-10-08 13:53:20 +0530

    add function completed

--- add.js
+++ add.js
@@ -1,5 +1,29 @@
 function add(str = "//;\n1;2") {
-  console.log(str);
+  const hasDelimiter = str.startsWith("//");
+  let delmtr = ",";
+
+  if (hasDelimiter) {
+    delmtr = str[2];
+  }
+  const strWithoutDelmtr = str.slice(3).replace("\n", "");
+
+  const numbers = strWithoutDelmtr.split(delmtr);
+
+  let count = 0;
+  numbers.forEach((num) => {
+    const number = Number(num);
+    if (!number) {
+      throw Error("Found non numeric character");
+    }
+
+    if (number < 0) {
+      throw Error("Found negative number");
+    }
+
+    count += number;
+  });
+
+  return count;
 }
 
-add();
+console.log(add());
