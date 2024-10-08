1b121ca68ac5000dd313e722fff06569affb4acb
abhinav khanduja <abhinavkhanduja@abhinavs-MacBook-Pro.local>
2024-10-08 14:04:52 +0530

    test: should add positive numbers with delimiter

--- add.test.js
+++ add.test.js
@@ -5,4 +5,8 @@ describe("add", function () {
   it("should add positive numbers", function () {
     expect(add("1,2,3")).to.equal(6);
   });
+
+  it("should add positive numbers with delimiter", function () {
+    expect(add("//|1|2\n|3\n|10")).to.equal(16);
+  });
 });
